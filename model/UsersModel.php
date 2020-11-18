a20faf57f98beb684f3122473cbb2878c2f0a25d
Brian Krupp <bkrupp@bw.edu>
2020-11-18 12:52:58 -0500

    Cleaned up UserModel code with documentation and also using static Database

--- model/UsersModel.php
+++ model/UsersModel.php
@@ -6,32 +6,51 @@ require_once(__DIR__ . "/types/User.php");
 require_once(__DIR__ . "/types/Response.php");
 
 class UsersModel {
-
   static public function authenticateUser($username, $password) : Response {
-    $db = new Database();
-
     $sql = "SELECT userId FROM tblUsers WHERE userName = ? AND guid = ?";
-
-    $result = $db->executeSql($sql, "ss", array($username, $password));
-
-    return new Response($result[0], $db->lastError);
+    $result = Database::executeSql($sql, "ss", array($username, $password));
+    return new Response($result[0], Database::$lastError);
   }
 
-  static public function createUser(User $user): Response {
-    $db = new Database();
+  /**
+   * Checks if a user exists given a username
+   * @param $username
+   * @return bool true if user exists, false otherwise
+   */
+  static public function doesUserExist($username) : bool {
+    $sql = "SELECT userId FROM tblUsers WHERE userName = ?";
+    $result = Database::executeSql($sql, "s", array($username));
+    return sizeof($result) > 0;
+  }
 
+  /**
+   * Creates a user give a username and a pin
+   * @param User $user
+   * @return bool true on success, false otherwise
+   */
+  static public function createUser(User $user): bool {
     $sql = "INSERT INTO tblUsers(userName, pin) VALUES (?, ?)";
+    Database::executeSql($sql, "ss", array($user->userName, $user->pin));
+    return ! isset(Database::$lastError);
+  }
 
-    $db->executeSql($sql, "si", array($user->userName, $user->pin));
 
-    return new Response(null, $db->lastError);
+  /**
+   * Updates a user pin to a new value
+   * @param User $user
+   * @return bool true on success, false otherwise
+   */
+  static public function updatePin(User $user): bool {
+    $sql = "UPDATE tblUsers SET pin = ? WHERE userName = ?";
+    Database::executeSql($sql, "ss", array($user->pin, $user->userName));
+    return ! isset(Database::$lastError);
   }
 
   static public function checkPinAndUser(User $user): Response {
     $response = new Response();
     $db = new Database();
 
-    $sql = "SELECT pin FROM tblUsers WHERE userName = ? AND pin = ?";
+    $sql = "SELECT userId FROM tblUsers WHERE userName = ? AND pin = ?";
 
     $results = $db->executeSql($sql, "si", array($user->userName, $user->pin));
 
@@ -45,24 +64,34 @@ class UsersModel {
     return $response;
   }
 
-  static public function updateVerifiedFlag(User $user): Response {
-    $db = new Database();
+
+  /**
+   * Sets whether or not a user is verified.
+   * @param User $user
+   * @param bool $isVerified
+   * @return bool true on success, false otherwise
+   */
+  static public function updateVerifiedFlag(User $user, bool $isVerified): bool {
+    $verified = 0;
+    if ($isVerified) {
+      $verified = 1;
+    }
 
     $sql = "UPDATE tblUsers SET accountValidated = ? WHERE userName = ?";
-
-    $db->executeSql($sql, "is", array(1, $user->userName));
-
-    return new Response(null, $db->lastError);
+    Database::executeSql($sql, "is", array($verified, $user->userName));
+    return ! isset(Database::$lastError);
   }
 
-  static public function addGuid(User $user, $GUID): Response {
-    $db = new Database();
-
+  /**
+   * Sets the GUID for a given user
+   * @param User $user
+   * @param $GUID
+   * @return bool true on success, false otherwise
+   */
+  static public function setGUID(User $user, $GUID): bool {
     $sql = "UPDATE tblUsers SET GUID = ? WHERE userName = ?";
-
-    $db->executeSql($sql, "ss", array($GUID, $user->userName));
-
-    return new Response(null, $db->lastError);
+    Database::executeSql($sql, "ss", array($GUID, $user->userName));
+    return ! isset(Database::$lastError);
   }
 
 }
