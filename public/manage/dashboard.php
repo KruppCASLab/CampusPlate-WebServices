af26689835d16e366a671585c3ba6ef339eee016
Brian Krupp <bkrupp@bw.edu>
2021-03-08 09:55:26 -0500

    Added button to allow reservations and listings to be udpated.

--- public/manage/dashboard.php
+++ public/manage/dashboard.php
@@ -54,10 +54,18 @@ $listings = ListingsController::get($listingRequest)->data;
   <title>CampusPlate | Manage</title>
 </head>
 <body>
+
 <div class="container min-vh-100 h-100" id="login">
   <h1 class="display-4"> Dashboard</h1>
-  <h5 class="subtitle" style="color:#<?=$selectedFoodStop->hexColor?>"><span class="badge" style="background-color:#<?=$selectedFoodStop->hexColor?>; border-radius:50%"><?=$selectedFoodStop->foodStopNumber?></span> <?=$selectedFoodStop->name?></h5>
-  <hr />
+  <h5 class="subtitle" style="color:#<?=$selectedFoodStop->hexColor?>"><span class="badge" style="background-color:#<?=$selectedFoodStop->hexColor?>; border-radius:50%"><?=$selectedFoodStop->foodStopNumber?></span> <?=$selectedFoodStop->name?> <div class="float-end">
+          <button class="btn btn-primary" onclick="window.location='dashboard.php?foodstop=<?=$selectedFoodStop->foodStopId?>'"><span class="glyphicon glyphicon-refresh"></span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                  <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+              </svg> Update</button>
+      </div></h5>
+
+
+    <hr />
   <!-- Modal -->
   <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
