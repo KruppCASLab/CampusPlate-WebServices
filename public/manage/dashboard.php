9b31f0f5ade272e4beed27bb10f30fe61ca5f6dc
Brian Krupp <bkrupp@bw.edu>
2021-03-16 08:46:33 -0400

    Added logo and title to login and dashboard page

--- public/manage/dashboard.php
+++ public/manage/dashboard.php
@@ -117,32 +117,41 @@ $listings = ListingsController::get($listingRequest)->data;
 
 <div class="container min-vh-100 h-100" id="login">
     <div class="row">
-        <h1 class="display-4"> Dashboard</h1>
-        <h5 class="subtitle" style="color:#<?= $selectedFoodStop->hexColor ?>">
-            <span class="badge" style="background-color:#<?= $selectedFoodStop->hexColor ?>; border-radius:50%">
-              <?= $selectedFoodStop->foodStopNumber ?>
-            </span> <?= $selectedFoodStop->name ?>
+        <h1 class="display-4">
+            <img class="img-fluid" style="height:80px" src="images/icon.png"/> Dashboard
             <div class="float-end">
-                <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#placeOrderModal">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                         class="bi bi-plus-circle" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                    </svg>
-                    Add Order
-                </button>
-
-                <button class="btn btn-primary"
-                        onclick="window.location='dashboard.php?foodstop=<?= $selectedFoodStop->foodStopId ?>'">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                         class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
-                        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
-                    </svg>
-                    Update
-                </button>
+            <span class="display-6">
+            <span class="subtitle"
+                  style="color:#<?= $selectedFoodStop->hexColor ?>"><?= $selectedFoodStop->name ?></span>
+            <span class="badge"
+                  style="background-color:#<?= $selectedFoodStop->hexColor ?>; border-radius:50%"><?= $selectedFoodStop->foodStopNumber ?></span>
+                </span>
             </div>
-        </h5>
+        </h1>
+        <div>
+
+        <div class="float-end">
+            <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#placeOrderModal">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     class="bi bi-plus-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                </svg>
+                Add Order
+            </button>
+
+            <button class="btn btn-primary"
+                    onclick="window.location='dashboard.php?foodstop=<?= $selectedFoodStop->foodStopId ?>'">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                    <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+                </svg>
+                Update
+            </button>
+        </div>
+        </div>
+
     </div>
 
 
