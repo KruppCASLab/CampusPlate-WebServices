5f2be49663e95fa3d770d0c229eecffc31b7e272
Brian Krupp <bkrupp@bw.edu>
2021-09-09 14:58:38 -0400

    Added basic reporting functionality

---
+++ model/ReportingModel.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once(__DIR__ . "/Database.php");
+require_once(__DIR__ . "/types/Reservation.php");
+require_once(__DIR__ . "/types/FoodStop.php");
+
+class ReportingModel {
+    static private function getLastWeekTimestamp() : int {
+        return time() - (7 * 24 * 60 * 60);
+    }
+
+    static public function getTotalItemsRecovered() : int {
+        $sql = "SELECT count(*) as total FROM tblReservations WHERE status = ? OR status = ? ";
+        $results = Database::executeSql($sql, "ii", array(Reservation::$RESERVATION_STATUS_FULFILLED, Reservation::$RESERVATION_STATUS_ON_DEMAND));
+        return $results[0]["total"];
+    }
+
+    static public function getItemsRecoveredLastWeek() : int {
+        $sql = "SELECT count(*) as total FROM tblReservations WHERE (status = ? OR status = ?) AND timeCreated > ? ";
+        $lastWeekTimestamp = self::getLastWeekTimestamp();
+        $results = Database::executeSql($sql, "iii", array(Reservation::$RESERVATION_STATUS_FULFILLED, Reservation::$RESERVATION_STATUS_ON_DEMAND, $lastWeekTimestamp));
+        return $results[0]["total"];
+    }
+
+    static public function getItemsPerDay() {
+        $sql = "SELECT DATE(FROM_UNIXTIME(timeCreated)) as createdDate, COUNT(*) as numPerDate FROM tblReservations WHERE status = ? or status = ? GROUP BY DATE(FROM_UNIXTIME(timeCreated)) ORDER BY createdDate";
+        $results = Database::executeSql($sql, "ii", array(Reservation::$RESERVATION_STATUS_FULFILLED, Reservation::$RESERVATION_STATUS_ON_DEMAND));
+        return $results;
+    }
+
+}
---
+++ public/manage/reporting.php
@@ -0,0 +1,128 @@
+<?php
+require_once(__DIR__ . "/../../lib/Security.php");
+require_once(__DIR__ . "/../../lib/Session.php");
+require_once(__DIR__ . "/../../model/types/FoodStop.php");
+require_once(__DIR__ . "/../../model/types/User.php");
+require_once(__DIR__ . "/../../model/types/Reservation.php");
+require_once(__DIR__ . "/../../model/types/Listing.php");
+require_once(__DIR__ . "/../../model/ReportingModel.php");
+require_once(__DIR__ . "/../../model/AuthorizationModel.php");
+
+if (!Session::isSessionValid() ) {
+    header('Location: ' . "index.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+            crossorigin="anonymous"></script>
+
+    <script src="js/main.js"></script>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <script src='https://cdn.plot.ly/plotly-2.4.2.min.js'></script>
+    <title>CampusPlate | Manage</title>
+</head>
+<body>
+
+<div class="container min-vh-100 h-100" id="login">
+    <?php
+
+    if (AuthorizationModel::isAdmin(Session::getSessionUserId())) {
+    ?>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link" href="dashboard.php">Dashboard</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="reporting.php">Reporting</a>
+        </li>
+        <li class="nav-item ">
+            <a class="nav-link disabled" href="#">Users</a>
+        </li>
+    </ul>
+    <?php
+    }
+    else {
+        die("You are not authorized to view this page.");
+    }
+    ?>
+
+    <div class="row mt-3">
+        <h1 class="display-4">
+            <img class="img-fluid" style="height:80px" src="../images/icon.png"/> Reporting
+        </h1>
+    </div>
+    <?php
+        $total = ReportingModel::getTotalItemsRecovered();
+        $lastWeek = ReportingModel::getItemsRecoveredLastWeek();
+    ?>
+
+    <h1 class="mt-3">Overall Food Recovery</h1>
+    <div class="row mt-3">
+        <div class="col-md-6 col-sm-12 text-center ">
+            <h1 class="display-1 mainColor"><?=$total?></h1>
+            <p>Items Recovered Since Inception</p>
+        </div>
+        <div class="col-md-6 col-sm-12 text-center">
+            <h1 class="display-1 mainColor"><?=$lastWeek?></h1>
+            <p>Items Recovered Last Week</p>
+        </div>
+    </div>
+    <div id="byDate">
+    </div>
+    <script>
+        <?php
+            $results = ReportingModel::getItemsPerDay();
+            ?>
+        let data = [
+            {
+                x: [
+                    <?php
+                    for ($i = 0; $i < sizeof($results); $i++) {
+                        echo "'" . $results[$i]["createdDate"] . "'";
+                        if ($i < sizeof($results) - 1) echo ",";
+                    }
+
+                    ?>],
+                y: [
+                    <?php
+
+                    for ($i = 0; $i < sizeof($results); $i++) {
+                        echo $results[$i]["numPerDate"];
+                        if ($i < sizeof($results) - 1) echo ",";
+                    }
+                    ?>],
+                type: 'bar',
+                marker: {
+                    color: '#EBB500'
+
+                }
+            }
+        ];
+        Plotly.newPlot("byDate", data);
+    </script>
+
+
+
+
+    <hr/>
+
+
+
+
+
+
+    <div style="height:50px"></div>
+    <hr/>
+    <div style="height:50px"></div>
+
+
+</div>
+</body>
+</html>
