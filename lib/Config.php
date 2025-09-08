83d6b0bf26c753ce2c33a87a7d18b3257522af62
Natalie Qiu <natalieqiu5@gmail.com>
2025-09-08 17:05:48 -0400

    changed config to point to my local server and adjusted foodstopsnodel to do managed and reservable

--- lib/Config.php
+++ lib/Config.php
@@ -2,7 +2,8 @@
 
 class Config {
     static private function loadConfig() {
-        $config = parse_ini_file("/var/www/cp/config.cfg", true);
+        //$config = parse_ini_file("/var/www/cp/config.cfg", true);
+        $config = parse_ini_file("/home/nsq5/cp/config.cfg", true);
         return $config;
     }
 
