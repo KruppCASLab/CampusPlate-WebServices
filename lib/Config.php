2675d4909749b60490fa3ba6a78e4c1b715d7761
Natalie Qiu <natalieqiu5@gmail.com>
2025-10-06 17:52:33 -0400

    switch baseurl back to prod

--- lib/Config.php
+++ lib/Config.php
@@ -2,7 +2,9 @@
 
 class Config {
     static private function loadConfig() {
+        //prod
         $config = parse_ini_file("/var/www/cp/config.cfg", true);
+        //dev
         //$config = parse_ini_file("/home/nsq5/cp/config.cfg", true);
         return $config;
     }
