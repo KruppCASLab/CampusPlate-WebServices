043da3b719f586d9fa70f0ae8fee67b43161201a
Brian Krupp <briankrupp@icloud.com>
2025-10-15 15:58:39 -0400

    Updated config to use a relative directory for development

--- lib/Config.php
+++ lib/Config.php
@@ -1,11 +1,9 @@
 <?php
 
 class Config {
+    static private $path = __DIR__ . "/../config.cfg";
     static private function loadConfig() {
-        //prod
-        $config = parse_ini_file("/var/www/cp/config.cfg", true);
-        //dev
-        //$config = parse_ini_file("/home/nsq5/cp/config.cfg", true);
+        $config = parse_ini_file(Config::$path, true);
         return $config;
     }
 
